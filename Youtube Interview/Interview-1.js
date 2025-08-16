600ac681809ea32fa1d1fa290ee62e823c276b65
saniya7474 <saniyaalase7757@gmail.com>
2025-08-16 21:14:22 +0530

    done

---
+++ Youtube Interview/Interview-1.js
@@ -0,0 +1,121 @@
+//  Interview Quesions
+
+//   const arr=[,,,]
+//   console.log(arr.length) //3
+
+ function abc (a,b,c=0){
+
+ }
+
+ function xyz (a,b=0,c){
+
+ }
+
+ function pqr(a=0,b,c){
+
+ }
+
+ console.log(abc.length)//2
+ console.log(xyz.length)//1
+ console.log(pqr.length)//0
+
+//  const arr1=[];
+//  const arr=[];
+
+//  console.log(arr==arr1)  //false
+//  console.log(arr===arr1) //false
+
+
+  if([])
+ {
+      console.log("I am Saniya")
+  }else
+ {
+     console.log("I am Absent")
+ }
+   
+// (I am Saniya)
+
+
+  if([] == true)
+ {
+      console.log("I am Saniya")
+  }else
+ {
+     console.log("I am Absent")
+ }
+ //I am Absent
+
+
+
+ console.log( [] ? true:false)  //true
+
+ console.log('1'+2+2) //122
+
+console.log(2+2+'1'+2+2) //4122
+
+
+const arr = ['saniya' , 'niya']
+
+const first = arr[0];
+const second = arr[1];
+
+let flag = false;
+for(let char of second){
+    if(first.includes(char)){
+        flag = true;
+        break;
+    }
+}
+
+console.log(flag ? "present":"no match char");//present 
+
+
+
+var c= 200;
+{
+    var c = 400
+}
+
+let d=c;
+
+{
+let d = -400
+}
+
+console.log(d)//400
+
+// var c= 200;
+// {
+//     let c = 400
+// }
+
+// let d=c;
+
+// {
+// let d = -400
+// }
+
+// console.log(d)//200
+
+
+
+// let obj = {
+//     a: {
+//         b: undefined
+//     }
+// }
+
+// console.log(obj.a?.b?.c?.d??"Saniya") //Saniya  because left hand side value is undefinde so this take right hand side value so output will be Saniya
+
+
+var lmn = 25;
+if(function f(){}){
+    lmn = lmn+typeof f;
+}
+
+console.log(lmn);
+// JavaScript Array Methods
+//What is the difference between every() and some() methods
+//shallow copy and deep copy
+//handle asynchronous code in three ways 1.callback function 2.promise 3.async&await
