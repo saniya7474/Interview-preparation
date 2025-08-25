dd88bcda79767dd57c0f56ad744bca89aa97c298
saniya7474 <saniyaalase7757@gmail.com>
2025-08-25 20:43:34 +0530

    InterviewN0 3 done

---
+++ Youtube Interview/InterviewNo-3.js
@@ -0,0 +1,15 @@
+//What is difference between virtual dom and real dom
+// what is use of es6 ?
+
+//difference between control and uncontrol component in react?
+//what is the prop drilling in react
+//Can we create custom Hooks in React? & When should I create a custom hook in React?
+//what is the use of strict mode in react i.e react.strictmode in react?
+
+
+
+
+
+
+
+//React js is a framework or libraray ==> It is a libraray
