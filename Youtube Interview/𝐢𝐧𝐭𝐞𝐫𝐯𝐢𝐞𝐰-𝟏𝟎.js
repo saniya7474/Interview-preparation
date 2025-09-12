99b652000d97053522542f3b08fe6d68b3fa8794
saniya7474 <saniyaalase7757@gmail.com>
2025-09-12 17:20:06 +0530

    Interview No-10 Done

---
+++ Youtube Interview/𝐢𝐧𝐭𝐞𝐫𝐯𝐢𝐞𝐰-𝟏𝟎.js
@@ -0,0 +1,28 @@
+
+// https://www.youtube.com/watch?v=WM7X-8ZewUk&t=271s
+
+let array=[10,4,9,2,40,50];
+console.log(Math.min.apply(Math,array));//2
+
+//What is react.js
+//What is Hooks in react
+//What is the use of UseEffect?
+//What is the use of useState?
+//What are the features of ReactJS? 
+//==>JSX(JavaScript Syntax Extension) or XML.
+    //Virtual DOM.
+    //One-way Data Binding.
+//What is the JSX?
+//What is the Component?
+//What is the different between Real-dom and Virtual-dom
+//React Native
+//What is Javascript?
+//Can you explain map method and filter method
+//What is the difference between map and forEach method?
+//In react what is the use of props?
+//What is the diffrence between state and props?
+//What is the react-route?
+//What is HTML?
+//Can you explain html layouts?
+//What is the diffrence between Strong Tag and Bold Tag?
+//What are the type of CSS? ==>Inline , Internal and External CSS
